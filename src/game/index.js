ab8e76aaaecf3d0b968df9ffa5700f63d9763ad1
Shutaro Takimoto <shu@tak.im>
2020-08-23 10:03:56 +0900

    chore: add comments

--- src/game/index.js
+++ src/game/index.js
@@ -1,30 +1,37 @@
+// Battle
+export { Game_BattlerBase } from './battlerBase';
+  export { Game_Battler } from './battler';
+    export { Game_Actor } from './actor';
+    export { Game_Actors } from './actors';
+    export { Game_Enemy } from './enemy';
+export { Game_Unit } from './unit';
+  export { Game_Party } from './party';
+  export { Game_Troop } from './troop';  
 export { Game_Action } from './action';
 export { Game_ActionResult } from './actionResult';
-export { Game_Actor } from './actor';
-export { Game_Actors } from './actors';
-export { Game_Battler } from './battler';
-export { Game_BattlerBase } from './battlerBase';
-export { Game_Character } from './character';
-export { Game_CharacterBase } from './characterBase';
-export { Game_CommonEvent } from './commonEvent';
-export { Game_Enemy } from './enemy';
-export { Game_Event } from './event';
-export { Game_Follower } from './follower';
-export { Game_Followers } from './followers';
-export { Game_Interpreter } from './interpreter';
-export { Game_Item } from './item';
+
+// Map
 export { Game_Map } from './map';
+export { Game_CharacterBase } from './characterBase';
+  export { Game_Character } from './character';
+    export { Game_Player } from './player';
+    export { Game_Event } from './event';
+    export { Game_Follower } from './follower';
+    export { Game_Followers } from './followers';
+    export { Game_Vehicle } from './vehicle';
+
+// Common
+export { Game_CommonEvent } from './commonEvent';
+export { Game_Item } from './item';
 export { Game_Message } from './message';
-export { Game_Party } from './party';
 export { Game_Picture } from './picture';
-export { Game_Player } from './player';
 export { Game_Screen } from './screen';
 export { Game_SelfSwitches } from './selfSwitches';
 export { Game_Switches } from './switches';
+export { Game_Timer } from './timer';
+export { Game_Variables } from './variables';
+
+// System
 export { Game_System } from './system';
 export { Game_Temp } from './temp';
-export { Game_Timer } from './timer';
-export { Game_Troop } from './troop';
-export { Game_Unit } from './unit';
-export { Game_Variables } from './variables';
-export { Game_Vehicle } from './vehicle';
+export { Game_Interpreter } from './interpreter';
