318180d39ac7803a49e681b2a0b8b392452ed004
Shutaro Takimoto <shu@tak.im>
2020-09-12 17:26:54 +0900

    chore: configure webpack to copy static js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const CircularDependencyPlugin = require('circular-dependency-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+// const WriteFilePlugin = require("write-file-webpack-plugin");
 const path = require('path');
 
 module.exports = {
@@ -10,9 +12,18 @@ module.exports = {
   },
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'static/js'),
+    path: path.resolve(__dirname, 'project/js'),
   },
   plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: "src/main.js", to: path.resolve(__dirname, "project/js") },
+        { from: "src/plugins.js", to: path.resolve(__dirname, "project/js") },
+        { from: "src/libs", to: path.resolve(__dirname, "project/js/libs") }
+      ]
+    }),
+    // new WriteFilePlugin(),
+
     new CircularDependencyPlugin({
       exclude: /node_modules/,
       failOnError: true,
